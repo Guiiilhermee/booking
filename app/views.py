f72833174ef4619538d787e2079964f7cb5d15ac
Guiiilhermee rodrigues <guilherme_rodrigues91@hotmail.com>
2023-12-28 22:33:55 +0000

    Added new bootstrap template, migrations, urls and photos

--- app/views.py
+++ app/views.py
@@ -1,6 +1,66 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.contrib import messages
+from .models import Appointment
+from django.views.generic import ListView
 
 class HomeTemplateView(TemplateView):
     template_name = "index.html"
+    
+    def post(self, request):
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        
+        email = EmailMessage(
+            subject = f"{name} from barber.",
+            body=message,
+            from_email=settings.EMAIL_HOST_USER,
+            to=[settings.EMAIL_HOST_USER],
+            reply_to=[email]
+        )
+        email.send()
+        return HttpResponse("Email sent successfully")
+    
+class AppointmentTemplateView(TemplateView):
+    template_name = 'appointment.html'
+    
+    def post(self, request):
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        email = request.POST.get("email")
+        mobile = request.POST.get("mobile")
+        message = request.POST.get("request")
+        
+        appointment = Appointment.objects.create(
+            first_name=fname,
+            last_name=lname,
+            email=email,
+            phone=mobile,
+            request=message,            
+        )
+        
+        appointment.save()
+        
+        messages.add_message(request, messages.SUCCESS, f"Thanks {fname} for making an appointment with us! We will email you ASAP!")
+        return HttpResponseRedirect(request.path)
+    
+class ManageAppointmentTemplateView(ListView):
+    template_name = "manage-appointments.html"
+    model = Appointment
+    context_object_name = "appointments"
+    login_required = True
+    paginate_by = 3
+    
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        appointments = Appointment.objects.all()
+        context.update({
+            
+        }) 
+        return context
+    
